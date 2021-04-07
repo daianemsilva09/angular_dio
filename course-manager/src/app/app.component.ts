5de353d66e8ec02764ac18849d2b8e37b78726c1
Daiane Silva <daianemsilva09@gmail.com>
2021-04-06 23:18:32 -0300

    commit aula Trabalhando com módulos e componentes

--- course-manager/src/app/app.component.ts
+++ course-manager/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'course-';
+  title = 'course';
+
+  name: string = 'Dai';
+
+
 }
