fc947f3e2e9023b8533a41d101ba19306597579b
Daiane Silva <daianemsilva09@gmail.com>
2021-04-07 21:13:24 -0300

    Aula 05

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from "@angular/forms";
+import { CourseListComponent } from './courses/course-list.component';
 @NgModule({
   declarations: [
     AppComponent,
--- course-manager/src/app/courses/course-list.component.ts
+++ course-manager/src/app/courses/course-list.component.ts
@@ -1,14 +1,39 @@
 import { Component } from "@angular/core";
+import { Course } from "./course";
 
 
 
 @Component({
     selector: 'app-course-list',
-    template: '<h2> Course List </h2>'
+    templateUrl: './course-list.component.html'
 
 })
 
 export class CourseListComponent {
+
+    courses: Course[] = [];
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: 'angular: Forms',
+                imageUrl: '',
+                price: 99.99,
+                code: 'XPS-8796',
+                duration: 120,
+                rating: 5.4
+            },
+            {
+                id: 2,
+                name: 'angular: HTTP',
+                imageUrl: '',
+                price: 9.99,
+                code: 'LKL-1094',
+                duration: 80,
+                rating: 4
+            }
+        ]
+    }
 
 }
 
